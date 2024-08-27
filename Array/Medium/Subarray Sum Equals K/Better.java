eb9cb52fb682e0ea59c5a3b9608eaf4109328c90
Tulsi-bhushan <117098319+Tulsi-Bhushan@users.noreply.github.com>
2024-08-27 20:56:58 +0530

    Better approach with O(n^2)

---
+++ Array/Medium/Subarray Sum Equals K/Better.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int count=0;
+        for(int i =0;i<n;i++){
+            int sum=0;
+            for(int j =i;j<n;j++){
+                sum+=nums[j];
+                }
+                if(sum==k) count++;
+            }
+        }
+        return count;
+    }
+}
+
+//time complexity reduces to O(n^2) by removing the inner loop 
